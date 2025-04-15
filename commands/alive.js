292f509d1cb0aa63c1a38c74d51439725dbb431f
DavMac751 <davidmac985@gmail.com>
2025-04-15 12:05:35 +0300

    Update alive.js

--- commands/alive.js
+++ commands/alive.js
@@ -1,17 +1,17 @@
-// commands/menu.js
+// commands/alive.js
 const fs = require("fs");
 const path = require("path");
 
 module.exports = {
-  name: "menu",
-  description: "Displays the bot's menu",
-  execute: async (sock, msg, args) => {
-    const imagePath = path.join(__dirname, "../public/menu.jpg");
+  name: "alive",
+  description: "Bot status check",
+  execute: async (sock, msg) => {
+    const imagePath = path.join(__dirname, "../public/main.jpg");
     const buffer = fs.readFileSync(imagePath);
 
     await sock.sendMessage(msg.key.remoteJid, {
       image: buffer,
-      caption: `*🤡 COSMO-MD BOT MENU 🤡*\n\n1. .alive\n2. .google <query>\n3. .ask <question>\n4. .play <song>\n5. .movie <title>\n6. .mp4 <url>\n7. .img <search>\n8. .games\n9. .rank\n10. .block <@user>\n11. .unblock <@user>\n...and more!`,
+      caption: `*🤖 COSMO-MD is Alive and Working!*\n\n- Prefix: \`.\`\n- Version: 3.0.0\n- Developer: Jack (DavMac751)\n- Type \`.menu\` to view all commands.`,
     });
   },
 };
